e1e0a63823ddb8d862a5e1c39dd4d0d5c4194128
elenajmr <elena.rosner@htl-salzburg.ac.at>
2021-03-23 10:22:56 +0100

    refac: fixed spacing

--- Assets/MyGame/Scripts/Obstacle.cs
+++ Assets/MyGame/Scripts/Obstacle.cs
@@ -2,36 +2,33 @@
 
 public class Obstacle : MonoBehaviour
 {
-Rigidbody2D rb;
-[SerializeField] private float MoveSpeed;
+    Rigidbody2D rb;
+    [SerializeField] private float MoveSpeed;
 
-private void Awake()
-{
-    rb = GetComponent<Rigidbody2D>();
-}
-
-// Update is called once per frame
-void Update()
-{
-    //if obstacle's position x is < -15f it will be destroyed
-    if(transform.position.x < -15f)
+    private void Awake()
     {
-        Destroy(gameObject);
-}
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         //if obstacle's position x is < -15f it will be destroyed
+        if(transform.position.x < -15f)
+        {
+            Destroy(gameObject);
+        }
         //if obstacle's position x is < -15f it will be destroyed
         if (transform.position.x > 15f)
         {
             Destroy(gameObject);
         }
-
     }
 
-
-
-private void FixedUpdate()
-{
+    private void FixedUpdate()
+    {
 
     rb.velocity = Vector2.left * MoveSpeed;
 
-}
+    }
 }
